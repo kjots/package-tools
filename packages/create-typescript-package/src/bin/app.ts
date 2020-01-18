9e039c4d0692a44382442bfccf068d8f0041c334
Karl Juri Ots <kjots@kjots.org>
2020-01-18 16:06:36 +1100

    feat(create-typescript-package): update to get app options from `bin/args` module of `package-creator` package

--- packages/create-typescript-package/src/bin/app.ts
+++ packages/create-typescript-package/src/bin/app.ts
@@ -1,44 +1,12 @@
 import yargs from 'yargs';
 
-import { defaults } from '@kjots/package-creator';
+import { getOpts, options } from '@kjots/package-creator/dist/bin/args';
 
 import { createTypescriptPackage } from '..';
 
 const argv = yargs
   .usage('Usage: $0 [options]')
-  .options({
-    help: {
-      alias: 'h'
-    },
-    output: {
-      alias: 'o',
-      describe: 'Project output directory',
-      nargs: 1,
-      default: defaults.output
-    },
-    name: {
-      alias: 'n',
-      describe: 'Project name',
-      nargs: 1,
-      default: defaults.name
-    },
-    description: {
-      alias: 'd',
-      describe: 'Project description',
-      nargs: 1
-    },
-    keywords: {
-      alias: 'k',
-      describe: 'Project keywords',
-      array: true,
-      default: defaults.keywords
-    }
-  })
+  .options(options)
   .argv;
 
-createTypescriptPackage({
-  output: argv.output as string,
-  name: argv.name as string,
-  description: argv.description as string,
-  keywords: argv.keywords as Array<string>
-});
+createTypescriptPackage(getOpts(argv));
