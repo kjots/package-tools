079c3bc69759a69a2adbb91c7890c3f535488d3b
Karl Juri Ots <kjots@kjots.org>
2020-01-18 16:07:06 +1100

    feat(create-typescript-monorepo-package): update to get app options from `bin/args` module of `package-creator` package

--- packages/create-typescript-monorepo-package/src/bin/app.ts
+++ packages/create-typescript-monorepo-package/src/bin/app.ts
@@ -1,25 +1,13 @@
 import yargs from 'yargs';
 
+import { getOpts, options } from '@kjots/package-creator/dist/bin/args';
+
 import { createTypescriptMonorepoPackage, defaults } from '..';
 
 const argv = yargs
   .usage('Usage: $0 [options]')
   .options({
-    help: {
-      alias: 'h'
-    },
-    output: {
-      alias: 'o',
-      describe: 'Package output directory',
-      nargs: 1,
-      default: defaults.output
-    },
-    name: {
-      alias: 'n',
-      describe: 'Package name',
-      nargs: 1,
-      default: defaults.name
-    },
+    ...options,
     monorepo: {
       alias: 'm',
       describe: 'Monorepo name',
@@ -31,26 +19,12 @@ const argv = yargs
       describe: 'Package references',
       array: true,
       default: defaults.references
-    },
-    description: {
-      alias: 'd',
-      describe: 'Package description',
-      nargs: 1
-    },
-    keywords: {
-      alias: 'k',
-      describe: 'Package keywords',
-      array: true,
-      default: defaults.keywords
     }
   })
   .argv;
 
 createTypescriptMonorepoPackage({
-  output: argv.output as string,
-  name: argv.name as string,
+  ...getOpts(argv),
   monorepo: argv.monorepo as string,
-  description: argv.description as string,
-  keywords: argv.keywords as Array<string>,
   references: argv.references as Array<string>
 });
