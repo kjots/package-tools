f0a941bf12bf1ef9bfcc6ce84c0bf916f5d3d10f
Karl Juri Ots <kjots@kjots.org>
2020-01-21 23:38:35 +1100

    chore(create-typescript-package): refactor TypeScript config to compile from and into package directory

--- packages/create-typescript-package/index.ts
+++ packages/create-typescript-package/index.ts
@@ -2,7 +2,7 @@ import { resolve } from 'path';
 
 import { Opts, packageCreator } from '@kjots/package-creator';
 
-const templateZip = resolve(__dirname, '../template.zip');
+const templateZip = resolve(__dirname, './template.zip');
 
 export function createTypescriptPackage(opts: Opts) {
   packageCreator(templateZip, opts);
