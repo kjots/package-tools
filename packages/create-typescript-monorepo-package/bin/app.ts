c3e39dea570298be61c3f73bd1b425c26fd9b9c0
Karl Juri Ots <kjots@kjots.org>
2020-01-21 23:40:27 +1100

    chore(create-typescript-monorepo-package): refactor TypeScript config to compile from and into package directory

--- packages/create-typescript-monorepo-package/bin/app.ts
+++ packages/create-typescript-monorepo-package/bin/app.ts
@@ -1,6 +1,7 @@
+#!/usr/bin/env node
 import yargs from 'yargs';
 
-import { getOpts, options } from '@kjots/package-creator/dist/bin/args';
+import { getOpts, options } from '@kjots/package-creator/bin/args';
 
 import { createTypescriptMonorepoPackage } from '..';
 
